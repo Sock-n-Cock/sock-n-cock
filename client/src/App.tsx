22606b82938b28bd5ba833da1041f38e245f8f6e
doshq <matvey4ikk@gmail.com>
2026-04-04 01:04:16 +0300

    Change styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,25 @@ import Editor, { type OnMount } from '@monaco-editor/react';
 
 function App() {
   const editorRef = useRef<Parameters<OnMount>[0] | null>(null);
+  const monacoRef = useRef<Parameters<OnMount>[1] | null>(null);
 
   return (
-    <div style={{ height: '100vh' }}>
-      <Editor
-        height="100%"
-        defaultLanguage="javascript"
-        theme="vs-dark"
-        onMount={(editor) => { editorRef.current = editor; }}
-      />
+    <div style={{ display: 'flex', height: '100vh', backgroundColor: '#1e1e1e', color: 'white', overflow: 'hidden' }}>
+      <div style={{ flex: 1, position: 'relative' }}>
+        <Editor
+          theme="vs-dark"
+          language="javascript"
+          onMount={(editor, monaco) => {
+            editorRef.current = editor;
+            monacoRef.current = monaco;
+          }}
+          options={{
+            fontSize: 15,
+            automaticLayout: true,
+            cursorSmoothCaretAnimation: 'on',
+          }}
+        />
+      </div>
     </div>
   );
 }
